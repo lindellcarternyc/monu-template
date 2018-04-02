a485245145ba8754432b1a2129df9c7d8048a8cc
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-02 19:56:47 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,59 @@
 import * as React from 'react'
 
 import { Navbar } from './components/navbar/Navbar'
-import { Button } from './components/button/Button'
-import { ActionButton } from './components/button/ActionButton'
+import { Hero } from './components/hero/Hero'
+import { ServicesPreview } from './components/card/ServicesPreview'
 
-class App extends React.Component {
+const Desktop = 944
+
+interface AppState {
+  isDesktop: boolean
+  menuActive: boolean
+}
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props) 
+    
+    this.state = {
+      isDesktop: false,
+      menuActive: false
+    }
+    
+  }
+
+  windowResize = () => {
+    const width = window.innerWidth
+    if ( width >= Desktop ) {
+      this.setState({ isDesktop: true })
+    } else {
+      this.setState({ isDesktop: false })
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.windowResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.windowResize)
+  }
+
+  toggleMenu = () => {
+    const menuActive = !this.state.menuActive
+    this.setState({ menuActive })
+  }
+
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Button text="Hello World" />
-        <ActionButton text="Hello Again" />
+      <div>
+        <Navbar 
+          screenSize={this.state.isDesktop ? 'desktop' : 'mobile'}
+          menuActive={this.state.menuActive}
+          toggleMenu={this.toggleMenu}
+          fixed
+        />
+        <Hero />
+        <ServicesPreview />
       </div>
     )
   }
