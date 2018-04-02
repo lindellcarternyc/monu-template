2e2df0479c22be33bb794787e5c7bdaec86d3a59
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-02 09:49:14 -0400

    added fontawesome

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
+import './assets/fontawesome-all.css'
 
 ReactDOM.render(
   <App />,
