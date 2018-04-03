c879524712530144454124ed5e80be102c52975a
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-03 13:18:44 -0400

    updated colors

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,7 +1,10 @@
-const Blue = '#3f7b9b'
+// const Blue = '#3f7b9b'
+const LightBlue = '#97b6c9'
+const Blue = '#0582ca'
 const DarkGrey = '#2e3438'
 
 export const Colors = {
+  LightBlue,
   Blue,
   DarkGrey
 }
