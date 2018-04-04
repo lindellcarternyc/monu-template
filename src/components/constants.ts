15bf9867a8da501aef25a5d612c210ab63e954bb
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-04 11:02:21 -0400

    added new constants

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,10 +1,14 @@
 // const Blue = '#3f7b9b'
-const LightBlue = '#97b6c9'
+// const LightBlue = '#97b6c9'
+const LightBlue = '#b3c1c9'
 const Blue = '#0582ca'
+// const MediumBlue = 
+const LightGrey = '#75848e'
 const DarkGrey = '#2e3438'
 
 export const Colors = {
   LightBlue,
   Blue,
+  LightGrey,  
   DarkGrey
 }
