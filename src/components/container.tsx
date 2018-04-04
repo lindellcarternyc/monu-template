1d8533acb02e8e747323fd0394f345eaa1d6a650
Lindell Carter <lindellcarternyc@gmail.com>
2018-04-04 11:05:53 -0400

    added padding to container

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -4,13 +4,25 @@ interface ContainerProps {
   children?: React.ReactNode
   backgroundColor?: string
   backgroundImage?: string
+  padding?: {
+    top?: string
+    bottom?: string
+    left?: string
+    right?: string
+  }
 }
 export const Container = (props: ContainerProps): JSX.Element => {
+  const padding: { [key: string]: string} = { 
+    paddingTop: props.padding && props.padding.top || '3rem',
+    paddingBottom: props.padding && props.padding.bottom || '3rem',
+    paddingLeft: props.padding && props.padding.left || '1rem',
+    paddingRight: props.padding && props.padding.right || '1rem'
+  }
   return (
     <div 
       style={{
         width: '100%',
-        padding: '3rem 1rem',
+        ...padding,
         overflow: 'hidden',
         position: 'relative',
         backgroundColor: props.backgroundColor
